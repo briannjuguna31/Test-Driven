de64d014a65f950d6f4e0546c3e071c6ef880cd7
Brian Njuguna <brian@ibuqacapital.com>
2023-07-22 17:30:50 +0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
-from datetime import time
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 
 
@@ -18,12 +19,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element("h1").text
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element("id_new_item")
-        self.assertEqual(inputbox.get_attribute("placeholer"), "Enter a to-to item")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
@@ -32,12 +33,13 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element("id_list_table")
-        rows = table.find_elements("tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows),
+            "New to-do item did not appear in table",
+        )
 
-        
-        
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
